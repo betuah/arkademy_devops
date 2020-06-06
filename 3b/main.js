c1f80fb44115f27d6dd40050bc4cd3fcc40e600e
Betuah Anugerah <betuahanugerah@gmail.com>
2020-06-06 21:09:14 +0700

    Add, Edit, Delete modal

--- 3b/main.js
+++ 3b/main.js
@@ -1,23 +1,39 @@
+const dataProduct = [
+    {'id': '1', 'id_cashier':'1', 'cashier': 'Pevita Pearce', 'product': 'latte', 'id_cat':'2','catagory': 'Drink', 'price': 10000},
+    {'id': '2', 'id_cashier':'2', 'cashier': 'Raisa Andriana', 'product': 'Cake', 'id_cat':'1','catagory': 'Food', 'price': 30000},
+    {'id': '3', 'id_cashier':'3', 'cashier': 'Betuah', 'product': 'Tai Tea', 'id_cat':'2','catagory': 'Drink', 'price': 15000}
+]
+
+const dataCatagory = [
+    {'id': 1, 'name': 'Food'},
+    {'id': 2, 'name': 'Drink'}
+]
+
+const dataCashier = [
+    {'id': 1, 'name': 'Pevita Pearce'},
+    {'id': 2, 'name': 'Raisa Andriana'},
+    {'id': 3, 'name': 'Betuah Anugerah'}
+]
+
+const edit = (id) => {
+    const res = dataProduct.find(data => data.id === id)
+    $('#cashierEdit').val(res.id_cashier)
+    $('#catagoryEdit').val(res.id_cat)
+    $('#productEdit').val(res.product)
+    $('#priceEdit').val(res.price)
+};
+
+const del = (id) => {
+    const ress = dataProduct.find(data => data.id === id)
+    console.log(ress)
+    $('#delData').html(`data ${ress.cashier} ID <span style="color: #FADC9C">${ress.id_cashier}</span>`)
+    // $('#delData').text(id)
+}
+
 $(document).ready(function(){
+
     
-    // Initial Data
-    const dataProduct = [
-        {'id': '1','cashier': 'Pevita Pearce', 'product': 'latte', 'catagory': 'Drink', 'price': 10000},
-        {'id': '2','cashier': 'Raisa Andriana', 'product': 'Cake', 'catagory': 'Food', 'price': 30000},
-        {'id': '3','cashier': 'Betuah', 'product': 'Tai Tea', 'catagory': 'Drink', 'price': 15000}
-    ]
-
-    const dataCatagory = [
-        {'id': 1, 'name': 'Food'},
-        {'id': 2, 'name': 'Drink'}
-    ]
-
-    const dataCashier = [
-        {'id': 1, 'name': 'Pevita Pearce'},
-        {'id': 2, 'name': 'Raisa Andriana'},
-        {'id': 3, 'name': 'Betuah Anugerah'}
-    ]
-
+    
     const htmlTable = dataProduct.map((data, i) => {
         return `<tr>
             <td>${i+1}</td>
@@ -26,8 +42,8 @@ $(document).ready(function(){
             <td>${data.catagory}</td>
             <td>${data.price}</td>
             <td>
-                <button onClick="edit(${data.id})" data-toggle="modal" data-target="#editModal" class="btn btn-lg" style="background-color:transparent; color: green"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                <button onClick="delete(${data.id})" class="btn btn-lg" style="background-color:transparent; color: red"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                <button onclick="edit('${data.id}')" data-toggle="modal" data-target="#editModal" class="btn btn-lg" style="background-color:transparent; color: green"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                <button onclick="del('${data.id}')" data-toggle="modal" data-target="#delModal" class="btn btn-lg" style="background-color:transparent; color: red"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
             </td>
         </tr>`
         
@@ -45,8 +61,9 @@ $(document).ready(function(){
     $('#myTable > tbody:last-child').append(htmlTable)
 
     $('#cashier').append(selectCashier)
-
     $('#catagory').append(selectCatagory)
+    $('#cashierEdit').append(selectCashier)
+    $('#catagoryEdit').append(selectCatagory)
 
     // Search
     $("#search").on("keyup", function() {
@@ -56,8 +73,4 @@ $(document).ready(function(){
         })
     })
 
-    //edit
-    function edit(id) {
-        alert(id)
-    }
 });
