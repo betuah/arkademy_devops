4b03f1bc73c32fccc98662c57fa616eb7f9111a3
Betuah Anugerah <betuahanugerah@gmail.com>
2020-06-06 20:35:47 +0700

    Update Soal 1

--- 1.js
+++ 1.js
@@ -1,13 +1,13 @@
 const soal1 = nilai => {
     const zeroSplit = nilai.toString().split(0)
-    const asd = []
+    const arr = []
 
     for (let i = 0; i < zeroSplit.length; i++) {
         const element = zeroSplit[i].split('')
-        asd.push(...element.sort())
+        arr.push(...element.sort())
     }
 
-    return asd.join('')
+    return arr.join('')
 }
 
 console.log(soal1(5956560159466056))
